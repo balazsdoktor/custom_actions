c8491ea2719e4a4bcfd449333c3b07be43b88939
Balazs Doktor <balazs.doktor@sanofi.com>
2024-07-22 12:57:34 +0200

    add output to javascript custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,8 +12,10 @@ function run(){
     // 2) Upload files
     const s3Uri = `s3://${bucket}`;
 
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
     core.notice('Welcome to my custom action.')
 }
 
